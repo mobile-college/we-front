dbe87e09f89bbc8a63786278dbd2279fed601b0d
zhangjiayi-bjhl <rayanddoctor@126.com>
2014-10-10 17:31:30 +0800

    update

--- edp-webserver-config.js
+++ edp-webserver-config.js
@@ -1,50 +1,70 @@
 exports.port = 8848;
 exports.directoryIndexes = true;
 exports.documentRoot = __dirname;
+
+/**
+ * 获取当前请求匹配的规则项，按规则定义的先后顺序匹配，先匹配到，先返回。没有匹配到，返回null。
+ *
+ * @param {Object} request 当前请求
+ * @return {?Array}
+ */
+function getRouter(pathname) {
+    var result;
+
+    var rules = require('./router').rules;
+    rules.forEach(function (item) {
+        if (item.pattern.test(pathname)) {
+            result = item;
+        }
+    });
+
+    return result;
+}
+
 exports.getLocations = function () {
     return [
-        { 
-            location: /\/$/, 
+        {
+            location: /\/$/,
             handler: home( 'index.html' )
         },
-        { 
-            location: /^\/redirect-local/, 
-            handler: redirect('redirect-target', false) 
+        {
+            location: /^\/redirect-local/,
+            handler: redirect('redirect-target', false)
         },
-        { 
-            location: /^\/redirect-remote/, 
-            handler: redirect('http://www.baidu.com', false) 
+        {
+            location: /^\/redirect-remote/,
+            handler: redirect('http://www.baidu.com', false)
         },
-        { 
-            location: /^\/redirect-target/, 
-            handler: content('redirectd!') 
+        {
+            location: /^\/redirect-target/,
+            handler: content('redirectd!')
         },
-        { 
-            location: '/empty', 
-            handler: empty() 
+        {
+            location: '/empty',
+            handler: empty()
         },
-        { 
-            location: /\.css($|\?)/, 
+        {
+            location: /\.css($|\?)/,
             handler: [
                 autocss()
             ]
         },
-        { 
-            location: /\.less($|\?)/, 
+        {
+            location: /\.less($|\?)/,
             handler: [
                 file(),
                 less()
             ]
         },
-        { 
-            location: /\.styl($|\?)/, 
+        {
+            location: /\.styl($|\?)/,
             handler: [
                 file(),
                 stylus()
             ]
         },
-        { 
-            location: /^.*$/, 
+        {
+            location: /^.*$/,
             handler: [
                 file(),
                 proxyNoneExists()
